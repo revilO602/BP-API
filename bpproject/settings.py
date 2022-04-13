bcf2e90f3669e49c189c1bf33945532975cd3679
Oliver Leontiev <leontiev.oliver@gmail.com>
2022-04-13 12:58:37 +0200

    development env variable

--- bpproject/settings.py
+++ bpproject/settings.py
@@ -21,7 +21,6 @@ ENV_VARS = dotenv_values(".env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -63,9 +62,9 @@ INSTALLED_APPS = [
     'django.contrib.gis',
 
     # Support apps
-    'drf_spectacular', # Generates OpenApi3 swagger documentation
+    'drf_spectacular',  # Generates OpenApi3 swagger documentation
     'rest_framework',
-    'rest_framework_gis', # geographical serializers
+    'rest_framework_gis',  # geographical serializers
     'corsheaders',
     'rest_framework_simplejwt',
 ]
@@ -106,7 +105,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'bpproject.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -155,7 +153,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -168,7 +165,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -249,6 +245,6 @@ EMAIL_USE_TLS = True
 
 GOOGLE_API_KEY = ENV_VARS.get('GOOGLE_API_KEY')
 
-# if DEBUG:
-   # GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, ENV_VARS.get('GDAL_PATH'))
-   # GEOS_LIBRARY_PATH = os.path.join(BASE_DIR, ENV_VARS.get('GEOS_PATH'))
+if ENV_VARS.get('DEVELOPMENT') == 'True':
+    GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, ENV_VARS.get('GDAL_PATH'))
+    GEOS_LIBRARY_PATH = os.path.join(BASE_DIR, ENV_VARS.get('GEOS_PATH'))
